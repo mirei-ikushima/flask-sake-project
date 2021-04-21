7f31d680a59d31f493547ca715ce67a041374c6a
A-PS1999 <sarnoldparra@gmail.com>
2021-04-21 20:08:08 +0100

    'Seventeenth commit: Tiny commit. Edited emails.py to make sending emails asynchronous using threading.'

--- app/emails.py
+++ app/emails.py
@@ -1,7 +1,14 @@
+from app import create_app
 from flask import render_template
 from flask_mail import Message
 from . import mail
+from threading import Thread
 import os
+
+
+def async_email(create_app, msg):
+    with create_app().app_context():
+        mail.send(msg)
 
 
 def send_email(subject, template, recipients, **kwargs):
@@ -10,4 +17,4 @@ def send_email(subject, template, recipients, **kwargs):
     msg = Message(subject, sender=sender, recipients=[recipients])
     msg.body = render_template(template + ".txt", **kwargs)
     msg.html = render_template(template + ".html", **kwargs)
-    mail.send(msg)
+    Thread(target=async_email, args=(create_app, msg)).start()
